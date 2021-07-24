d14f5f4294e91a50525268f7c276daf250899254
Kevin <kevin@gigaclub.net>
2021-07-24 06:21:58 +0200

    imp(gigaclub_requests): added constrains for unique waiting checks

--- models/gc_request.py
+++ models/gc_request.py
@@ -1,4 +1,5 @@
 from odoo import fields, models, api
+from odoo.exceptions import ValidationError
 
 
 class GCRequest(models.Model):
@@ -8,3 +9,10 @@ class GCRequest(models.Model):
     receiver_id = fields.Reference(selection=[("gc.user", "gc.user")])
     sender_id = fields.Reference(selection=[("gc.user", "gc.user")])
     state = fields.Selection(selection=[("waiting", "Waiting"), ("accepted", "Accepted"), ("denied", "Denied")])
+
+    @api.constrains("receiver_id", "sender_id", "state")
+    def _check_duplicate_request(self):
+        for record in self:
+            for compareRec in self.search([("id", "!=", record.id)]):
+                if record.receiver_id == compareRec.receiver_id and record.sender_id == compareRec.sender_id and record.state == "waiting" and record.state == compareRec.state:
+                    raise ValidationError("waiting requests should be unique!")
